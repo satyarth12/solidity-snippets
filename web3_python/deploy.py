6ae4aa3121542c209b237eee7c136fefef38ce7d
Satyarth <srk.satyarth.1@gmail.com>
2021-12-01 02:16:21 +0530

    Added contract interact module

--- web3_python/deploy.py
+++ web3_python/deploy.py
@@ -7,6 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ganache_url = "HTTP://127.0.0.1:7545"
+web3_ = Web3(Web3.HTTPProvider(ganache_url))
+
+chain_id = 1337
+address = os.getenv("ADDRESS")
+private_key = os.getenv("PRIVATE_KEY")
+
 
 def compile(contract):
     contract_name = contract
@@ -49,20 +56,10 @@ def compile(contract):
     return abi, bytecode
 
 
-# compile(contract_name="SimpleStorage.sol")
-
 def deploy_contract(abi_, bytecode_):
     """Connection with ganache and deploying on it
     """
-
-    ganache_url = "HTTP://127.0.0.1:7545"
-    web3_ = Web3(Web3.HTTPProvider(ganache_url))
-    print(web3_.isConnected())
-
-    chain_id = 1337
-    address = os.getenv("ADDRESS")
-    private_key = os.getenv("PRIVATE_KEY")
-
+    print("DEPLYOING CONTRACT")
     contract_object = web3_.eth.contract(abi=abi_, bytecode=bytecode_)
 
     # Get the latest transaction on blockchain for nonce
@@ -77,15 +74,46 @@ def deploy_contract(abi_, bytecode_):
     })
 
     # Signing the txn_build
-    signed_txn = web3_.eth.account.sign_transaction(txn_build, private_key)
+    signed_txn = web3_.eth.account.sign_transaction(
+        txn_build, private_key)
 
     # Send the signed txn to blockchain
     txn_hash = web3_.eth.send_raw_transaction(signed_txn.rawTransaction)
 
     # Txn receipt of the deployed contract
     txn_receipt = web3_.eth.wait_for_transaction_receipt(txn_hash)
-    print("Contract Deployed At:", txn_receipt['contractAddress'])
+
+    print("CONTRACT DEPLOYED !!")
+    return txn_receipt['contractAddress'], abi_, nonce
+
+
+def contract_interact(cont_add, abi, nonce):
+    """Wroking with contracts
+    Call -> Simulate making the call and getting a return value. Doesn't make state change
+    Transact -> Make the state change
+    """
+    print("UPDATING CONTRACT")
+    contract_object = web3_.eth.contract(address=cont_add, abi=abi)
+
+    store_txn = contract_object.functions.store(15).buildTransaction({
+        'from': address,
+        'nonce': nonce+1,
+        'gas': 2000000,
+        'gasPrice': web3_.toWei('50', 'gwei')
+    })
+
+    sign_store_txn = web3_.eth.account.sign_transaction(
+        store_txn, private_key)
+
+    store_txn_hash = web3_.eth.send_raw_transaction(
+        sign_store_txn.rawTransaction)
+
+    txn_receipt = web3_.eth.wait_for_transaction_receipt(store_txn_hash)
+
+    print("CONTRACT UPDATED !!!")
+    print(contract_object.functions.retreive().call())
 
 
 abi, bytecode = compile(contract="SimpleStorage.sol")
-deploy_contract(abi_=abi, bytecode_=bytecode)
+cont_add, abi, nonce = deploy_contract(abi_=abi, bytecode_=bytecode)
+contract_interact(cont_add=cont_add, abi=abi, nonce=nonce)
